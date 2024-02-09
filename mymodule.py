749c3e62b9c780fd2b69264e2c0b3671764433a5
Jayasri2021 <jayasri.suresh2021@gmail.com>
2024-02-09 14:01:12 +0530

    Modules and packages concept

---
+++ mymodule.py
@@ -0,0 +1,10 @@
+def useful_func():
+    print("Using the usefull function")
+
+class UsefulClass():
+
+    def __init__(self,message):
+        self.message = message
+    
+    def report(self):
+        print(self.message)
---
+++ myprogram.py
@@ -0,0 +1,10 @@
+from mymodule import useful_func
+from mymodule import UsefulClass
+from mypackage.mysubmodule import my_sub_func
+
+useful_func()
+obj = UsefulClass("hello!")
+
+obj.report()
+
+my_sub_func()
